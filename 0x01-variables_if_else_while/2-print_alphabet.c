3222a4d2a718b8f374c2c5e8026e798290140908
judith <judithkiyegha@gmail.com>
2022-09-09 16:57:27 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - This is the Strating point of the program
+ * Return: main function
+ */
+int main(void)
+
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		printf("%c", c);
+		c++;
+	}
+	printf("\n");
+
+	return (0);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
