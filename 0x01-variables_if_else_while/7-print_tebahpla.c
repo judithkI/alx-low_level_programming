6596062a9afa28cde98e4929c2ad62bf8cfac151
judith <judithkiyegha@gmail.com>
2022-09-09 18:15:23 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This is the starting point of the program
+ * Return: main function
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+
+	{
+		printf("%c", c);
+		c--;
+	}
+	printf("\n");
+	return (0);
+
+}
+
+
+
+
