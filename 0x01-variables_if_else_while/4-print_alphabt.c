adbb700c9cc1d06c3bbf3f5b4da89739c1132b03
judith <judithkiyegha@gmail.com>
2022-09-09 17:33:43 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - This is the starting point of the program
+ * Return: main function
+ */
+int main(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+		if (c != 'q' && c != 'e')
+
+		{
+
+			printf("%c", c);
+
+		}
+		c++;
+	}
+	printf("\n");
+
+	return (0);
+
+}
+
