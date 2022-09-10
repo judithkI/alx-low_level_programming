39b8c6b570f8d79c8165fc30bf2319ce6a793de2
judith <judithkiyegha@gmail.com>
2022-09-09 18:01:37 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - This is the starting of the program
+ * description - print the number from 0 to 9 using only putchar()
+ * Return: main function
+ */
+int main(void)
+
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
+
+
+
