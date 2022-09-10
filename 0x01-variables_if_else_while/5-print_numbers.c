adafaf47ab1aefb70e2738a01c4cb4138b6bda95
judith <judithkiyegha@gmail.com>
2022-09-09 17:45:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - This is the starting point of the program
+ * Return: main function
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+	returnv (0);
+
+}
+
+
+
+
