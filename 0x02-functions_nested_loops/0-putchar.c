d39ef9916e8a597cb50bcda861b91d9375abbca1
judith <judithkiyegha@gmail.com>
2022-09-13 16:28:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
 
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: program that prints the alphabets
+ * followed by a new line
+ *
+ * Return: Exit int function with an int 0
+ */
+void print_alphabet(void)
+{
+	int aj;
+
+	for (aj = 'a'; aj <= 'z'; aj++)
+	{
+		_putchar(aj);
+	}
+	_putchar('\n');
+}
 
 
 
