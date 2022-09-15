bdef9b5ff7c5cdfcb0a38ee537fa76768e35afc7
judith <judithkiyegha@gmail.com>
2022-09-14 18:17:02 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,17 @@
 
 void print_alphabet_x10(void);
 {
-int count = 0;
-char letter;
-while (count++ <= 9)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-_putchar('\n');
-}
+	char c, i;
+	for (i = 0; i < 10; i++)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
 }
 
 
