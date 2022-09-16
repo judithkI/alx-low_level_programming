112d4137505dbd0010b4bc9fcdd57bf8a3c58e04
judith <judithkiyegha@gmail.com>
2022-09-15 17:13:58 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,17 +6,40 @@
 */
 int main(void)
 {
-int i;
 
-printf("Infinite loop incoming :(\n");
+	int i;
 
-i = 0;
-
-while (i < 10)
-{
-putchar(i);
-}
-
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+	/*
+	 * while (i < 10)
+	 *
+	 * {
+	 *
+	 * Infinite loop - No increase of variable
+	 *
+	 * putchar(i);
+	 *
+	 * }
+	 *
+	 */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
