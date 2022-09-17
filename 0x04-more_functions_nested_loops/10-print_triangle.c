7c185937c687f54b710cc85cc6b3ac0dec45dbde
judith <judithkiyegha@gmail.com>
2022-09-16 21:38:00 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (inc1 1; inc1 <= size; inc1++)
+		for (inc1 = 1; inc1 <= size; inc1++)
 		{
 			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
@@ -22,17 +22,13 @@ void print_triangle(int size)
 			
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
-			
-				putchar('#');
-		
+				putchar('#');	
 			}
 			
 			if (inc1 == size)
 			{
-			
 				continue;
 			}
-
 		
 			putchar('\n');
 		}
