21b84c2d9885bec193ee0c2f721156663d9e7f85
judith <judithkiyegha@gmail.com>
2022-09-19 16:13:08 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,45 +1,16 @@
 #include "main.h"
-
 /**
- * print_alphabet - function to print abc
- * Return: 0
- *
- */
-
+* print_alphabet - function to print abc
+*
+* Return: 0
+*/
 void print_alphabet(void)
 {
-char c = 'a';
-while (c <= 'z')
-{
-_putchar(c);
-}
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 _putchar('\n');
-{
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+void num(int r, int n);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
