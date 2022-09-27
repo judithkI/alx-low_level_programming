6d01ed2dbff7222d9e2f2012ad337b9ef31586b3
judith <judithkiyegha@gmail.com>
2022-09-27 14:08:00 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,19 +2,25 @@
 
 /**
  * print_chessboard - print the chessboard, 2d array
- * @a: two dimension array to print
- * Return: void 
+ * @a: 2d array of chars
+ * Return: void
  */
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	for (i = 0; i < 8; i++)
+	/*variables*/
+	int n, j;
+	n = 0;
+	while (n < 8) /*repeat number*/
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+		j = 0;
+		while (j < 8)
+		{
+			_putchar (a[n][j]);
+			j++;
+		}
+		_putchar ('\n'); /*print new line*/
+		n++;
 	}
 }
 
