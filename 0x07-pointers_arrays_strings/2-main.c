1f8058bdcd5dbaf68d5679857b1cee4e0289ef3f
judith <judithkiyegha@gmail.com>
2022-09-26 20:25:27 -0700

    2-main.c

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strchr(char *s, char c);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "hello";
+	char *f;
+
+	f = _strchr(s, '1');
+
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
+	return (0);
+}
+
