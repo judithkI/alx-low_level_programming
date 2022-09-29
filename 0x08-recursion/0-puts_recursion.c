1c4445d849050def01eefc207c2fd5c34611cfdc
judith <judithkiyegha@gmail.com>
2022-09-28 19:21:22 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,11 +3,10 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
- * Return: returns void.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0');
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
