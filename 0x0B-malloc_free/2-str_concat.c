365dbfef4d782b86199dffb9cc878cb088ecb53f
judith <judithkiyegha@gmail.com>
2022-10-03 11:27:06 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include < stdlib.h>
+#include <string.h>
+
 /**
   * str_concat - a function that concatenate two strings.
   * @s1: the first string.
   * @s2: the second string.
   * Return: returns apointer to the concat
   */
+
 char *str_concat(char *s1, char *s2)
 {
 	int i;
