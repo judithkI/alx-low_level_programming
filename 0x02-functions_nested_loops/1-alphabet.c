0a04fe0c571f9f2906888f9c3ad40a85f9ac8d65
judith <judithkiyegha@gmail.com>
2022-10-03 14:50:00 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-* print_alphabet - function to print abc
-*
-* Return: 0
-*/
+  * print_alphabet - function to print abc
+  *
+  * Return: 0
+  */
+
 void print_alphabet(void)
 {
 	char c;
@@ -12,5 +13,8 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
+
 _putchar('\n');
+
 }
+
