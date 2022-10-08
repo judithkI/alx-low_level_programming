28599858a6d1ce6be0bf5c56101095f6e5f15f2e
judith <judithkiyegha@gmail.com>
2022-10-08 04:22:27 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,7 +7,7 @@
  *  @nmemb: The number of elements.
  *  @size: The byte size of each array element.
  *
- *  Return: if nmemb = , size = 0, or the function fails - NULL.
+ *  Return: if nmemb = 0, size = 0, or the function fails - NULL.
  *  Otherwise - a pointer to the allocated memory.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
