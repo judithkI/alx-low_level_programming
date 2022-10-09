b32e67fd19b04562ad9b4e50cdd39c6e89d6e803
judith <judithkiyegha@gmail.com>
2022-10-09 12:18:46 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+  * print_first - fuction to print something before main is executed
+  * @void: nothing
+  * Return: returns nothing.
+  */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
