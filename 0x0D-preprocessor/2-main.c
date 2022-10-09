dbfc1ff6b3a8347da89f1e2d3e709da7b53c7394
judith <judithkiyegha@gmail.com>
2022-10-09 09:13:10 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of this file if compiled.
+ * __FILE__: inbuilt macro that stores the filename.
+ * Return: returns an integer.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
