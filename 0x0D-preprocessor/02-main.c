571339b4fbc88976ec0f6558b48964a159be45f0
judith <judithkiyegha@gmail.com>
2022-10-09 09:07:08 -0700

    02-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of this file if compiled.
+ * Return: returns an integer.
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
