8b488e562abf497fe0c6a5fe7ccff529361ee8cb
judith <judithkiyegha@gmail.com>
2022-10-16 19:35:04 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - conputes the sum of all miltiples
+  * of 3 or 5 below 1024
+  * Return: Always 0 (sucess)
+  *
+  */
+int main(void)
+{
+	unsigned long int sum3, sun5, sum;
+	int i;
+
+
+	sum3 = 0;
+
+	sum5 = 0;
+
+	sum = 0;
+
+
+
+	for (i = 0; i < 1024; ++i)
+
+	{
+
+		if ((i % 3) == 0)
+
+		{
+
+			sum3 = sum3 + i;
+
+		} 
+		else if ((i % 5) == 0)
+
+		{
+
+			sum5 = sum5 + i;
+
+		}	
+
+	}	
+
+	sum = sum3 + sum5;
+
+	printf("%lu\n", sum);
+
+	return (0);
